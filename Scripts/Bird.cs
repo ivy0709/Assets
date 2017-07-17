0c8f44d1cbf1ddf94cb0d6d9a51fddfd54652b34
fulina <fuln12@126.com>
2017-07-17 22:18:36 +0800

    the first part

---
+++ Scripts/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private int frameCounts;
+    private float time;
+    private int curFrameIdx;
+
+    // Use this for initialization
+    void Start () {
+        frameCounts = 10;
+        time = 0.0f;
+        curFrameIdx = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        time += Time.deltaTime;
+		if(time >= 1.0f/frameCounts)
+        {
+            int factorOffset = curFrameIdx % 3;
+            gameObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(factorOffset * 0.33333f, 0));
+            ++curFrameIdx;
+            time -= 1.0f / frameCounts;
+        }
+    }
+}
